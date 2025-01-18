f4a4dfb7d3ec9531ea200df64a41c97c27b68e44
Haziq <haziqiskandar443@gmail.com>
2025-01-19 00:33:00 +0800

    Changes to configuration. Sidebar moved to index.html

--- testingGround/tailwind.config.js
+++ testingGround/tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: ["'Inter'", 'sans-serif'],
+    },
   },
   plugins: [],
 }
